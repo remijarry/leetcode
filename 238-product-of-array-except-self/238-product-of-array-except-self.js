72981e8ab6595b9e966d79ff25a82b893fc93d25
Rémi <rem_ay@hotmail.fr>
2022-09-30 18:43:33 +1300

    Time: 216 ms (18.09%), Space: 56.2 MB (17.38%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let prefix = 1;
+
+    let arr = [1];
+    for (let i = 0; i < nums.length - 1; i++) {
+        prefix *= nums[i];
+        arr.push(prefix);
+    }
+
+    let postfix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        arr[i] = arr[i] * postfix;
+        postfix *= nums[i];
+    }
+
+    return arr;
+};
