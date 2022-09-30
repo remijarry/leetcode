a66bbc46ec35560e0f57d671d652555b541e2607
Rémi <rem_ay@hotmail.fr>
2022-09-30 18:22:22 +1300

    Time: 102 ms (74.71%), Space: 43.1 MB (30.70%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let difference = (target - nums[i]);
+        if (map.has(difference)) {
+            return [map.get(difference), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+};
