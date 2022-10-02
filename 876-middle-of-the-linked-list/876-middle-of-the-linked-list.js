df79c567ee45a7c5c243b37d8762f19d8d03187c
Rémi <rem_ay@hotmail.fr>
2022-10-03 08:50:43 +1300

    Time: 76 ms (74.49%), Space: 42.2 MB (26.93%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head.next;
+    
+    while (fast) {
+        slow = slow.next;
+        fast = fast.next;
+        if (fast) {
+            fast = fast.next;
+        }
+    }
+    
+    return slow;
+};
