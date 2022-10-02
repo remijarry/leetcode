ba4f53145a9750f721a4958d554d56d391fe1374
Rémi <rem_ay@hotmail.fr>
2022-10-03 08:38:49 +1300

    Time: 122 ms (40.49%), Space: 45.4 MB (28.48%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+
+    while (fast) {
+        slow = slow.next;
+        fast = fast.next;
+        if (fast) {
+            fast = fast.next;
+        }
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+};
