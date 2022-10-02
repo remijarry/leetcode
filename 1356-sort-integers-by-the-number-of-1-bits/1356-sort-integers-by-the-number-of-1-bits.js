6720cfc7c8003fb313fe5cdc511814c5eabe0897
Rémi <rem_ay@hotmail.fr>
2022-10-03 08:58:06 +1300

    Time: 147 ms (49.48%), Space: 44.6 MB (69.65%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -0,0 +1,17 @@
+function sortByBits(numbers) {
+    const map = {};
+
+    for (let i = 0; i < numbers.length; i++) {
+        let counter = 0;
+        let bit = numbers[i];
+        
+        while(bit > 0) {
+            counter += (bit & 1);
+            bit = bit >> 1;
+        }
+
+        map[numbers[i]] = counter;
+    }
+
+    return numbers.sort((a, b) => map[a] - map[b] || a - b);
+}
